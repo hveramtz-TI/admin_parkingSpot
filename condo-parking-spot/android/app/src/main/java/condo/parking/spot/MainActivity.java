2faae9753cf58dc7358f97c999954aefb567f492
Hector Vera <hveramtz.inf@gmail.com>
2025-02-01 16:25:41 -0300

    delete

--- condo-parking-spot/android/app/src/main/java/condo/parking/spot/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package condo.parking.spot;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- condo-parking-spot/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'condo.parking.spot',
-  appName: 'condo-parking-spot',
-  webDir: 'dist'
-};
-
-export default config;
